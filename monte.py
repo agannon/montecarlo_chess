4ac963b41aec461c8badacca36a5a4aebb40a6d7
andrewgannon <agannon94@gmail.com>
2020-06-04 18:01:47 -0400

    adds json loading

--- monte.py
+++ monte.py
@@ -14,9 +14,10 @@ new_board = chess.Board()
 
 class Node:
 
-    def __init__(self, board_epd, player, parent):
+    def __init__(self, board_epd, player, parent, last_move):
         self.wins = 0
         self.sims = 0
+        self.last_move = last_move
         self.player = player
         self.boardstate = board_epd
         self.children = {}
@@ -55,7 +56,7 @@ class Node:
         if move:
             bottom.legal_moves_remaining.pop(bottom.legal_moves_remaining.index(move))
             board.push(move)
-            new_node = Node(board.epd(), 'W' if bottom.player == 'B' else 'B', bottom)
+            new_node = Node(board.epd(), 'W' if bottom.player == 'B' else 'B', bottom, move.uci())
             bottom.children[move] = new_node
             return new_node
         return bottom
@@ -115,6 +116,7 @@ class Node:
                 string += f'\173' \
                           f'"wins": {node.wins},' \
                           f'"sims": {node.sims},' \
+                          f'"last_move": "{node.last_move}",' \
                           f'"player": "{node.player}",' \
                           f'"boardstate": "{node.boardstate}",' \
                           f'"legal_moves_remaining": {[m.uci() for m in node.legal_moves_remaining]},' \
@@ -130,13 +132,30 @@ class Node:
 
         return string
 
+    @classmethod
+    def create_from_dict(cls, dictionary, parent):
+        stack = [(dictionary, parent)]
+        root = None
+        while stack:
+            d, p = stack.pop()
+            node = Node(d.get('boardstate'), d['player'], p, d.get('last_move'))
+            node.wins = d.get('wins')
+            node.sims = d.get('sims')
+            node.legal_moves_remaining = [chess.Move.from_uci(uci) for uci in d.get('legal_moves_remaining')]
+            node.total_legal_moves = d.get('total_legal_moves')
+            stack += list(zip(d.get('children'), [node] * len(d.get('children'))))
+            if not p:
+                root = node
+            else:
+                p.children[chess.Move.from_uci(node.last_move)] = node
+        return root
 
 class GameRunner:
     memory = 'saved_tree.json'
 
     def __init__(self):
         # self.player = 'W'
-        self.root = Node(new_board.epd(), 'W', None)
+        self.root = Node(new_board.epd(), 'W', None, '')
         self.node_pointer = self.root
 
     def run(self):
@@ -154,7 +173,7 @@ class GameRunner:
                 self.node_pointer = self.node_pointer.children.get(move)
             else:
                 # self.player = 'B'
-                new_node = Node(board.epd(), 'B', self.node_pointer)
+                new_node = Node(board.epd(), 'B', self.node_pointer, move.uci())
                 self.node_pointer.children[move] = new_node
                 self.node_pointer = new_node
             self.node_pointer.run_simulator()
@@ -169,9 +188,9 @@ class GameRunner:
         with open(self.memory, 'w') as file:
             file.write(self.root.serialize())
 
-    # def load(self):
-    #     with open(self.memory, 'rb') as file:
-    #         self.root = pickle.load(file)
+    def load(self):
+        with open(self.memory, 'r') as file:
+            g.root = Node.create_from_dict(json.load(file), None)
 
 
 g = GameRunner()
